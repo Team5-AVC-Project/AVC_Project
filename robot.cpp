3af9849ee4f8eb99445a6a26da9bde7ee2b69639
James Ybanez <ybanezjames17@gmail.com>
2020-06-10 18:58:18 +1200

    Finished core
    Changed the detection column, so now it detects the row of pixels in the column closest to the robot. Which made it easier for the robot to handle turns and curves. Changed the speed at which the robot turns depending on whether it has a difference between the middle white line and the middle pixel. Added an extra else if statement that allows the robot to continue moving forward when it just about reaches the flag where it loses the white pixels.

--- robot.cpp
+++ robot.cpp
@@ -8,43 +8,43 @@ int main(){
 	}
     double vLeft = 10.0;
     double vRight = 10.0;
-    //takePicture();
-    //SavePPMFile("i0.ppm",cameraView);
+    takePicture();
+    SavePPMFile("i0.ppm",cameraView);
     while(1){
 	  takePicture();
-	  double error = 0.0;
 	  double middle = 0.0;
-	  int errorNum = 0;
-	  double errorTotal = 0;
+	  int whiteNum = 0;
+	  double error = 0.0;
 	  for (int i = 0; i < 150; i++){
-		  int pix = get_pixel(cameraView, 50, i, 3);
+		  int pix = get_pixel(cameraView,99.99, i, 3);
 		  int isWhite;
 		  if(pix > 250){ 
 			  isWhite = 1;
-			  errorTotal = errorTotal + i;
-			  errorNum = errorNum + 1;
-			  error = i;
+			  error = error + i; 
+			  whiteNum = whiteNum + 1; // amount of white that occurs
 		  }
 		  else if (pix<=250){
 			  isWhite=0; 
 		  }
 		  std::cout<<isWhite<<" ";
 	  }
-	  middle = errorTotal/errorNum;
-	  double turn = 0;
-	  if (error > middle){
-		  turn = error - middle;
-		  vLeft = turn;
-		  vRight = -turn; 
+	  middle = error/whiteNum; //middle white line
+	  double differenceErrorMiddle = (100/2) - middle; 
+	  if (differenceErrorMiddle > 0){ //turns the robot to the right 
+		  vLeft = 10.00;
+		  vRight = 15.00;
 	  }
-	  else if (error < middle){
-		  turn = middle - error; 
-		  vLeft = -turn;
-		  vRight = turn;
+	  else if (differenceErrorMiddle < 0){ //turns the robot to the left
+		  vLeft = 15.00;
+		  vRight = 10.00;
 	  }
-	  else{
-		  vLeft = 10;
-		  vRight = 10;
+	  else if (differenceErrorMiddle == 0){ //moves straight
+		  vLeft = 10.00;
+		  vRight = 10.00;
+	  }
+	  else if (whiteNum == 0){ //moves robot straight when it's about to reach finish line
+		  vLeft = 10.00;
+		  vRight = 10.00;
 	  }
 	  
 	  std::cout<<std::endl;
