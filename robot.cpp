e207eb3792140fb65304e369e157e087c7f24e83
JeffPeake <66190965+JeffPeake@users.noreply.github.com>
2020-06-11 13:30:10 +1200

    Core update #2
    - added better turning system (by calculating dv=Kp · error) 
    - separated white line detection and speed calculation into separate function 'speedVariable'

--- robot.cpp
+++ robot.cpp
@@ -1,5 +1,7 @@
 #include "robot.hpp"
 
+double speedVariable();
+
 int main(){
 	if (initClientRobot() !=0){
 		std::cout<<" Error initializing robot"<<std::endl;
@@ -8,42 +10,12 @@ int main(){
     double vRight = 10.0;
     takePicture();
     SavePPMFile("i0.ppm",cameraView);
+    
     while(1){
-		  takePicture();
-	  double middle = 0.0;
-	  int whiteNum = 0;
-	  double error = 0.0;
-	  for (int i = 0; i < 150; i++){
-		  int pix = get_pixel(cameraView,50, i, 3); //get pixel from middle row of image
-		  int isWhite;
-		  if(pix > 250){ 
-			  isWhite = 1;
-			  error = error + i; 
-			  whiteNum = whiteNum + 1; // amount of white that occurs
-		  }
-		  else if (pix<=250){
-			  isWhite=0; 
-		  }
-		  std::cout<<isWhite<<" ";
-	  }
-	  middle = error/whiteNum; //middle white line
-	  double differenceErrorMiddle = (150/2) - middle; //how far from centre white line is
-	  if (differenceErrorMiddle > 0){ //turns the robot to the right 
-		  vLeft = 10.00;
-		  vRight = 15.00;
-	  }
-	  else if (differenceErrorMiddle < 0){ //turns the robot to the left
-		  vLeft = 15.00;
-		  vRight = 10.00;
-	  }
-	  else if (differenceErrorMiddle == 0){ //moves straight
-		  vLeft = 10.00;
-		  vRight = 10.00;
-	  }
-	  else if (whiteNum == 0){ //moves robot straight when it's about to reach finish line
-		  vLeft = 10.00;
-		  vRight = 10.00;
-	  }
+	  takePicture();
+	  
+		vLeft = 20.00 - speedVariable();
+		vRight = 20.00 + speedVariable();
 	  
 	  std::cout<<std::endl;
       setMotors(vLeft,vRight);   
@@ -52,4 +24,34 @@ int main(){
   } //while
 
 } // main
+
+double speedVariable(){
+	takePicture();
+	  double whiteMiddle = 0.0; // middle of white line
+	  int whiteNum = 0; //number of pixels in white line
+	  double whitePos = 0.0; // sum of positions that are white  
+	  
+	  for (int i = 0; i < 150; i++){ // search along the halfway line for the white line
+		  int pix = get_pixel(cameraView,50, i, 3);
+		  int isWhite;
+		  if(pix > 250){ 
+			  isWhite = 1;
+			  whitePos = whitePos + i; 
+			  whiteNum = whiteNum + 1;
+		  }
+		  else if (pix<=250){
+			  isWhite=0; 
+		  }
+		 std::cout<<isWhite<<" ";
+	  }
+	  if(whiteNum==0){ // if white line is gone then continue straight
+		return 0.0;
+	  }else{
+		whiteMiddle = whitePos/whiteNum; 
+	  }
+	  double error = (150/2) - whiteMiddle; // how far from middle white line is 
+	  double dv = 0.20 * error; //how much power to give wheels
+	  
+	  return dv;
+} // finds white line, calculates amount of error then calculates how much to turn (dv)
 
